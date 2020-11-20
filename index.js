2da6daaaca8895b5044c03b85bebd57100cac01d
Lucas Assmann <l.assmann.30@gmail.com>
2020-11-20 00:36:39 -0300

    multiple pages

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ const $ = require("cheerio")
 
 const start = async () => {
     try {
-        const html = await request("https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina/page7132");
-        const posts = $('div.content', html);
-
-        // const $ = cheerio.load(html)
-        console.log("***", posts.length)
-        const links = []
-
-        for (var i = 0; i < posts.length; i++) {
-            const postLinks = $('a', posts[i]);
-            for (var j = 0; j < postLinks.length; j++) {
-                if (!postLinks[j].attribs.href.includes('showthread') && !postLinks[j].attribs.href.includes('foros')) links.push({
-                    href: postLinks[j].attribs.href
-                })
+        const lastLinkHtml = await request("https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina");
+        const linkUrl = $('.first_last > a', lastLinkHtml)
+        // console.log('linkUrl', linkUrl)
+        const last = 7167;
+        const links = new Set();
+        for( var l = last; l > last - 20 ; l--) {
+            const html = await request(`https://foros.3dgames.com.ar/threads/942062-ofertas-online-argentina/page${l}`);
+            const posts = $('div.content', html);
+            for (var i = 0; i < posts.length; i++) {
+                const postLinks = $('a', posts[i]);
+                for (var j = 0; j < postLinks.length; j++) {
+                    if (!postLinks[j].attribs.href.includes('showthread') && !postLinks[j].attribs.href.includes('foros')) links.add(postLinks[j].attribs.href)
+                }
             }
         }
+
+
+
         console.log(links)
 
     } catch (error) {
